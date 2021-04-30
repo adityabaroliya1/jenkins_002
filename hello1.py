23a21499dcbaeef29380af3963fec33d49a97633
adityabaroliya1 <83391429+adityabaroliya1@users.noreply.github.com>
2021-04-30 09:50:04 +0530

    Add files via upload

---
+++ hello1.py
@@ -0,0 +1,41 @@
+print("aditya")
+
+if 5 > 2:
+  print("Five is greater than two!")
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+x = 5
+y = "Jyoti"
+print(x)
+print(y)
+
+x = 4       # x is of type int
+x = "Sriram" # x is now of type str
+print(x)
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+
+
+
+
+x = 5
+y = "Kundu"
+print(type(x))
+print(type(y))
+
+x = "Kudu"
+# is the same as
+x = 'Kundu'
+
+a = 4
+A = "Sriram"
+#A will not overwrite a
+
+
